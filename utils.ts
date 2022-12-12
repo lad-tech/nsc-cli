d4f51d7023c914cb5134de3d35c1a9660214444c
niki <blynskijno@lad24.ru>
2022-12-12 13:17:29 +0300

    fix: исправлен баг, если нет файла .nscignore

--- utils.ts
+++ utils.ts
@@ -2,16 +2,19 @@ import { readFile } from 'fs/promises';
 
 import { compile } from 'gitignore-parser';
 import * as path from 'path';
-
-declare namespace parser {}
-
 export async function parseIgnoreFile(location: string) {
   const str = await readFile(location, { encoding: 'utf8' });
   return compile(str);
 }
 
 export async function isIgnore(directoryPath: string, filepath: string) {
-  const ignoreFile = await parseIgnoreFile(path.join(directoryPath.toString(), `.nscignore`));
-  console.log(filepath, 'ignore =', ignoreFile.denies(filepath));
-  return ignoreFile.denies(filepath);
+  try {
+    const ignoreFile = await parseIgnoreFile(path.join(directoryPath.toString(), `.nscignore`));
+    console.log(filepath, 'ignore =', ignoreFile.denies(filepath));
+    return ignoreFile.denies(filepath);
+  } catch (e) {
+    console.error(e)
+    return false
+  }
+
 }
