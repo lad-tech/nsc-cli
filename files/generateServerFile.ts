ccbd4cebaa94c3c6996a37ada3bfc9a321f4e5dc
niki <blynskijno@lad24.ru>
2023-05-03 10:06:44 +0300

    fix: исправил генерацию евентов

--- files/generateServerFile.ts
+++ files/generateServerFile.ts
@@ -7,7 +7,7 @@ import {
   VariableDeclarationKind,
 } from 'ts-morph';
 import { FILE_EXTENTION, SERVICE_RUN_FILE_NAME, TOOLKIT_MODULE_NAME } from '../constants';
-import { MiddlewareFn, MiddlewareOptions } from '../interfaces';
+import { MiddlewareFn, MiddlewareOptions, ServiceSchema } from '../interfaces';
 import { isIgnore } from '../utils';
 
 export const generateServerFile: MiddlewareFn = async (opts: MiddlewareOptions): Promise<void> => {
@@ -17,21 +17,14 @@ export const generateServerFile: MiddlewareFn = async (opts: MiddlewareOptions):
     return;
   }
   const existsFile = project.addSourceFileAtPathIfExists(filePath);
+
   const shutdown: string | undefined = '';
   const hasEvents = Object.keys(schema.events?.list || {}).length > 0;
-  const methodImports: ImportDeclarationStructure[] = Object.keys(schema.methods).map(name => {
-    return {
-      kind: StructureKind.ImportDeclaration,
-      namedImports: [name],
-      moduleSpecifier: `./methods/${name}`,
-    };
-  });
-  methodImports.push({
-    kind: StructureKind.ImportDeclaration,
-    namedImports: hasEvents ? ['name', 'events'] : ['name'],
-    moduleSpecifier: `./${opts.schemaFileName}`,
-  });
+
+  const methodImports: ImportDeclarationStructure[] = getMethodsImports(schema, hasEvents, opts.schemaFileName);
+
   const methodNames = Object.keys(schema.methods);
+
   if (existsFile) {
     const imports = existsFile.getImportDeclarations();
     imports.forEach(i => {
@@ -48,27 +41,33 @@ export const generateServerFile: MiddlewareFn = async (opts: MiddlewareOptions):
       .getFunctionOrThrow('main')
       .getDescendantsOfKind(SyntaxKind.NewExpression)
       .find(ex => ex.getText().includes('Service'));
-    const ServiceArguments = service?.getDescendantsOfKind(SyntaxKind.ObjectLiteralExpression)?.[0]?.getProperties();
 
+    const ServiceArguments = service?.getDescendantsOfKind(SyntaxKind.ObjectLiteralExpression)?.[0]?.getProperties();
     const methodsArray = ServiceArguments?.find(
       arg => (arg.getStructure() as PropertyAssignmentStructure)?.name === 'methods',
     );
+
     methodsArray?.set({
       initializer: `[${methodNames.join(',')}]`,
     });
     const events = !!ServiceArguments?.find(
       arg => (arg.getStructure() as PropertyAssignmentStructure)?.name === 'events',
     );
-    if (hasEvents && !events) {
-      service?.getDescendantsOfKind(SyntaxKind.ObjectLiteralExpression)?.[0]?.addProperties('events');
+    //  если в схеме есть евенты
+    if (hasEvents) {
+      // в файле они отсутствуют
+      if (!events) {
+        service?.getDescendantsOfKind(SyntaxKind.ObjectLiteralExpression)?.[0]?.addProperties('events');
+      }
     } else {
       service?.getDescendantsOfKind(SyntaxKind.ObjectLiteralExpression)?.[0]?.getProperty('events')?.remove();
     }
-    await existsFile.save();
-    return;
-  }
+    console.log(service?.getDescendantsOfKind(SyntaxKind.ObjectLiteralExpression)?.[0]?.getText());
 
-  const initParams = `{
+    await existsFile.save();
+  } else {
+    //  создать новый файл
+    const initParams = `{
     name,
     brokerConnection,
     methods: [${methodNames.join(',')}],
@@ -81,50 +80,79 @@ export const generateServerFile: MiddlewareFn = async (opts: MiddlewareOptions):
           })
     }
   }`;
-  const file = project.createSourceFile(
-    filePath,
-    {
-      statements: [
-        {
-          kind: StructureKind.ImportDeclaration,
-          namedImports: ['Service'],
-          moduleSpecifier: TOOLKIT_MODULE_NAME,
-        },
-        {
-          kind: StructureKind.ImportDeclaration,
-          namedImports: ['connect'],
-          moduleSpecifier: 'nats',
-        },
-        ...methodImports,
-        {
-          kind: StructureKind.Function,
-          name: 'main',
-          isAsync: true,
-          statements: [
-            {
-              kind: StructureKind.VariableStatement,
-              declarationKind: VariableDeclarationKind.Const,
+    const file = project.createSourceFile(
+      filePath,
+      {
+        statements: [
+          {
+            kind: StructureKind.ImportDeclaration,
+            namedImports: ['Service'],
+            moduleSpecifier: TOOLKIT_MODULE_NAME,
+          },
+          {
+            kind: StructureKind.ImportDeclaration,
+            namedImports: ['connect', 'NatsConnection'],
+            moduleSpecifier: 'nats',
+          },
+          ...methodImports,
+          {
+            kind: StructureKind.Function,
+            name: 'main',
+            isAsync: true,
+            parameters: [
+              {
+                kind: StructureKind.Parameter,
+                name: 'broker',
+                type: 'NatsConnection',
+              },
+            ],
+            statements: [
+              {
+                kind: StructureKind.VariableStatement,
+                declarationKind: VariableDeclarationKind.Const,
 
-              declarations: [
-                {
-                  name: 'brokerConnection',
-                  initializer: 'await connect({ servers: [`localhost:4222`] })',
-                },
-              ],
-            },
-            '// Этот файл генерируется автоматически, этот вызов важен для генерации',
+                declarations: [
+                  {
+                    name: 'brokerConnection',
+                    initializer:
+                      '  broker ||\n' +
+                      '      (await connect({\n' +
+                      '        servers: [`localhost:4222`],\n' +
+                      '        maxReconnectAttempts: -1,\n' +
+                      '      }))',
+                  },
+                ],
+              },
+              '// Этот файл генерируется автоматически, этот вызов важен для генерации',
 
-            `await new Service(${initParams}).start();`,
-          ],
-        },
-      ],
-    },
-    {
-      overwrite: true,
-    },
-  );
+              `const service =  new Service(${initParams});`,
+              `await service.start();`,
+              `return service;`,
+            ],
+          },
+        ],
+      },
+      {
+        overwrite: true,
+      },
+    );
 
-  file?.addStatements(['main().catch(console.error);']);
-
-  await file.save();
+    await file.save();
+  }
 };
+
+function getMethodsImports(schema: ServiceSchema, hasEvents: boolean, schemaFileName: string) {
+  const methodImports: ImportDeclarationStructure[] = Object.keys(schema.methods).map(name => {
+    return {
+      kind: StructureKind.ImportDeclaration,
+      namedImports: [name],
+      moduleSpecifier: `./methods/${name}`,
+    };
+  });
+  methodImports.push({
+    kind: StructureKind.ImportDeclaration,
+    namedImports: hasEvents ? ['name', 'events'] : ['name'],
+    moduleSpecifier: `./${schemaFileName}`,
+  });
+  return methodImports;
+}
