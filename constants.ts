c3c988ef0520f20750ca12c75fa151d462c2fb29
niki <blynskijno@lad24.ru>
2024-02-06 12:14:08 +0300

    fix: Создание файла start.ts
    #Closes 10

--- constants.ts
+++ constants.ts
@@ -1,4 +1,5 @@
 export const INDEX_FILE_NAME = 'index';
+export const START_FILE_NAME = 'start';
 export const INTERFACES_FILE_NAME = 'interfaces';
 export const SERVICE_RUN_FILE_NAME = 'service';
 export const FILE_EXTENTION = '.ts';
