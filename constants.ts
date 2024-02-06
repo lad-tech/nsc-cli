b32973d8143b9169fc28ac8ca9d51007fa0088ab
niki <blynskijno@lad24.ru>
2024-02-06 10:42:15 +0300

    fix: В конструктор клиента добавить новый параметр loggerOutputFormatter
    #Closes 3

--- constants.ts
+++ constants.ts
@@ -3,3 +3,4 @@ export const INTERFACES_FILE_NAME = 'interfaces';
 export const SERVICE_RUN_FILE_NAME = 'service';
 export const FILE_EXTENTION = '.ts';
 export const TOOLKIT_MODULE_NAME = '@lad-tech/nsc-toolkit';
+export const TOOLBELT_MODULE_NAME = '@lad-tech/toolbelt';
--- files/generateIndexFile.ts
+++ files/generateIndexFile.ts
@@ -1,6 +1,12 @@
 import * as path from 'path';
 import { ImportSpecifierStructure, MethodDeclarationStructure, OptionalKind, StructureKind } from 'ts-morph';
-import { FILE_EXTENTION, INDEX_FILE_NAME, INTERFACES_FILE_NAME, TOOLKIT_MODULE_NAME } from '../constants';
+import {
+  FILE_EXTENTION,
+  INDEX_FILE_NAME,
+  INTERFACES_FILE_NAME,
+  TOOLBELT_MODULE_NAME,
+  TOOLKIT_MODULE_NAME,
+} from '../constants';
 import { MiddlewareFn, MiddlewareOptions } from '../interfaces';
 import { isIgnore } from '../utils';
 
@@ -72,10 +78,16 @@ export const generateIndexFile: MiddlewareFn = async (opts: MiddlewareOptions): 
           namedImports: importFields,
           moduleSpecifier: `./${schemaFileName}`,
         },
+        // import { Logs } from '@lad-tech/toolbelt';
         {
           kind: StructureKind.ImportDeclaration,
           namedImports: ['Client', 'Baggage', 'CacheSettings'],
           moduleSpecifier: TOOLKIT_MODULE_NAME,
+        },
+        {
+          kind: StructureKind.ImportDeclaration,
+          namedImports: ['Logs'],
+          moduleSpecifier: TOOLBELT_MODULE_NAME,
         },
         {
           kind: StructureKind.ImportDeclaration,
@@ -91,12 +103,13 @@ export const generateIndexFile: MiddlewareFn = async (opts: MiddlewareOptions): 
         {
           kind: StructureKind.Class,
           name: schema.name,
+
           ctors: [
             {
               kind: StructureKind.Constructor,
-              statements: `super({ broker, serviceName: name, baggage, cache ${hasEvents ? ',events' : ''} ${
-                schema.Ref?.$id ? ',Ref' : ''
-              } }); `,
+              statements: `super({ broker, serviceName: name, baggage, cache,loggerOutputFormatter ${
+                hasEvents ? ',events' : ''
+              } ${schema.Ref?.$id ? ',Ref' : ''} }); `,
               parameters: [
                 {
                   kind: StructureKind.Parameter,
@@ -113,6 +126,11 @@ export const generateIndexFile: MiddlewareFn = async (opts: MiddlewareOptions): 
                   kind: StructureKind.Parameter,
                   name: 'cache?',
                   type: 'CacheSettings',
+                },
+                {
+                  kind: StructureKind.Parameter,
+                  name: 'loggerOutputFormatter?',
+                  type: 'Logs.OutputFormatter',
                 },
               ],
             },
